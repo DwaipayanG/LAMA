f2694473b59fe6d3803e868884acdc0253e2128f
sukriti93em <141900702+sukriti93em@users.noreply.github.com>
2023-08-10 16:02:43 +0530

    Update EmployeeMasterController.java

--- backend/src/main/java/com/example/backend/controller/EmployeeMasterController.java
+++ backend/src/main/java/com/example/backend/controller/EmployeeMasterController.java
@@ -1,22 +1,29 @@
 package com.example.backend.controller;
 
 
+
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.backend.models.EmployeeMaster;
+import com.example.backend.services.EmployeeMasterService;
 
 
 @RestController
 @CrossOrigin("http://localhost:3000")
+
 public class EmployeeMasterController {
 	@Autowired
-	EmployeeMasterService employeeMasterService;
+	private EmployeeMasterService employeeMasterService;
 	
-	@PostMapping("/saveEmployeeMaster")
-	public EmployeeMaster saveEmployeeMaster(@RequestBody EmployeeMaster emp)
+	@PostMapping("/addEmployeeMaster")
+	public EmployeeMaster addEmployeeMaster(@RequestBody EmployeeMaster emp)
 	{
-		EmployeeMaster e = employeeMasterService.saveEmployeeMaster(emp);
+		EmployeeMaster e = employeeMasterService.addEmployeeMaster(emp);
 		return e;
 	}
 
