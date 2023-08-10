8f237dee446770d8fb8cd475d557ce086e9a0973
DwaipayanG <guhadwaipayan10@gmail.com>
2023-08-10 16:38:01 +0530

    feat: form in frontend for EmployeeMaster

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,5 +1,42 @@
+import axios from "axios";
+import { useState } from "react";
+
 export default function Login(){
+    const baseURL = "http://localhost:8080/addEmployee";
+
+    const [employeeId, setEmployeeId] = useState('');
+    const [employeeName, setEmployeeName] = useState('');
+
+    const employeeIdChangeHandler = (event) => {
+        setEmployeeId(event.target.value)
+    }
+
+    const employeeNameChangeHandler = (event) => {
+        setEmployeeName(event.target.value)
+    }
+
+    const submitActionHandler = (event) => {
+        axios
+            .post(baseURL, {
+                "employeeId": employeeId,
+                "employeeName": employeeName,
+            })
+            .then((response) => {
+                alert(response.data);
+            })
+            .catch((error) => {
+                alert(error);
+            })
+    }
+
     return (
-        <h3> Login </h3>
+        <div>
+            <h3> Employee Login </h3>
+             <form onSubmit={submitActionHandler}>
+                ID : <input type="text" value={employeeId} onChange={employeeIdChangeHandler} id="employeeId"/><br></br>
+                Name : <input type="text" value={employeeName} onChange={employeeNameChangeHandler} id="employeeName"/><br></br>
+               
+             </form>
+        </div>
     );
 };
