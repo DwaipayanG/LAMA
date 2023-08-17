2b198d753260718b12d223c477e933553cb600f3
DivyanshuTemp <throwawaydivyanshu@gmail.com>
2023-08-17 18:43:55 +0530

    java utesting

--- backend/src/test/java/com/example/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/example/backend/BackendApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan(basePackages="com")
-@SpringBootTest(classes= com.example.backend.LoanCardMasterTest.class)
+@SpringBootTest(classes= {com.example.backend.LoanCardMasterTest.class,com.example.backend.AdminControllerTest.class})
 class BackendApplicationTests {
 
 	@Test
