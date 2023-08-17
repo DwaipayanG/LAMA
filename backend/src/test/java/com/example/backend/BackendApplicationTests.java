b3065e83dc1fdc0bcc8448a550092e1cd3352438
sukriti93em <141900702+sukriti93em@users.noreply.github.com>
2023-08-17 17:43:17 +0530

    testing for itemsMaster

--- backend/src/test/java/com/example/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/example/backend/BackendApplicationTests.java
@@ -1,8 +1,12 @@
 package com.example.backend;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+
+
+@ComponentScan(basePackages = "com")
 @SpringBootTest
 class BackendApplicationTests {
 
