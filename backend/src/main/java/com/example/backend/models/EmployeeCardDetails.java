ba3224422b41dd011640827160ada13253a6b4e7
hrishk <hrishikeshmallick7@gmail.com>
2023-08-17 14:46:23 +0530

    Issue resolved

--- backend/src/main/java/com/example/backend/models/EmployeeCardDetails.java
+++ backend/src/main/java/com/example/backend/models/EmployeeCardDetails.java
@@ -12,8 +12,6 @@ import jakarta.persistence.IdClass;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name="employee_card_details")
