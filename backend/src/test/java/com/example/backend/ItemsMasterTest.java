ccd8379d8818d77a18dd58f17683ecf781f892e7
DivyanshuTemp <throwawaydivyanshu@gmail.com>
2023-08-18 13:09:18 +0530

    item master get testing

--- backend/src/test/java/com/example/backend/ItemsMasterTest.java
+++ backend/src/test/java/com/example/backend/ItemsMasterTest.java
@@ -1,16 +1,16 @@
 package com.example.backend;
 
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -18,13 +18,13 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 
-import com.example.backend.dao.ItemsMasterRepository;
-import com.example.backend.models.EmployeeIssueDetails;
 import com.example.backend.models.ItemsMaster;
-import com.example.backend.models.LoanCardMaster;
+import com.example.backend.services.EmployeeCardDetailsService;
+import com.example.backend.services.EmployeeIssueDetailsService;
+import com.example.backend.services.EmployeeMasterService;
 import com.example.backend.services.ItemsMasterService;
+import com.example.backend.services.LoanCardMasterService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
@@ -36,20 +36,26 @@ public class ItemsMasterTest {
 	private MockMvc mvc;
 	
 	@MockBean
+	private LoanCardMasterService loanCardMasterService;
+	
+	@MockBean
+	private EmployeeCardDetailsService employeeCarddetailsService;
+	
+	@MockBean
+	private EmployeeIssueDetailsService employeeIssueDetailsService;
+	
+	@MockBean
+	private EmployeeMasterService employeeMasterService;
+	
+	@MockBean
 	private ItemsMasterService itemsMasterService;
-	
-	@MockBean
-	private ItemsMasterRepository itemsMasterRepository;
-	
-	@MockBean
-	EmployeeIssueDetails employeeIssueDetails;
 	
 	ObjectMapper mapper = new ObjectMapper().findAndRegisterModules().disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
 	
 
 	
 	@Test
-	public void testsaveItemsMaster() throws Exception{
+	public void testGetAllCategory() throws Exception{
 		ItemsMaster itemsMaster = new ItemsMaster();
 		itemsMaster.setItemId("116732");
 		itemsMaster.setItemDescription("loan");
@@ -57,12 +63,14 @@ public class ItemsMasterTest {
 		itemsMaster.setItemMake("car");
 		itemsMaster.setItemCategory("personal");
 		itemsMaster.setItemValuation(22000);
-		Mockito.when(itemsMasterService.saveItemsMaster(ArgumentMatchers.any())).thenReturn(itemsMaster));
-		String json=mapper.writeValueAsString(itemsMaster);
-		MvcResult requestResult= mvc.perform(post("/saveItemsMaster").contentType(MediaType.APPLICATION_JSON).characterEncoding("utf-8").content(json).accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andReturn();
-		String result= requestResult.getResponse().getContentAsString();
-		System.out.print(result);
-		assertEquals(result,result);
+		List<String> allCategoryList = new ArrayList<>();
+		allCategoryList.add(itemsMaster.getItemCategory());
+		
+		Mockito.when(itemsMasterService.getAllCategory()).thenReturn(allCategoryList);
+		System.out.println("testing getting all categories.");
+		
+		mvc.perform(get("/getAllCategory").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+		.andExpect(jsonPath("$", Matchers.hasSize(1))).andExpect(jsonPath("$[0]", Matchers.equalTo(itemsMaster.getItemCategory())));
 	}			
 
 	
