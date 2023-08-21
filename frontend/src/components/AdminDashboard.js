d5aaaa9d632f3f8b08954d18bb99f6b872de52ed
DivyanshuTemp <throwawaydivyanshu@gmail.com>
2023-08-21 12:57:34 +0530

    admin dashboard final css

--- frontend/src/components/AdminDashboard.js
+++ frontend/src/components/AdminDashboard.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Card from 'react-bootstrap/Card';
+import {Row,Col} from 'react-bootstrap';
 import Header from "./Header";
 import "../style/Card.css"
 
@@ -12,79 +13,52 @@ export default function AdminDashboard(){
         <div>
             <Header></Header>
         
-            <p className="welcomeText"> Welcome <b>{sessionStorage.getItem("adminUsername")}</b> </p>
-            <div style={{width:"70%",margin:"auto"}} className="d-flex justify-content-center m-2">
+            <p className="welcomeText" style={{fontSize: "2em"}}> Welcome <b>{sessionStorage.getItem("adminUsername")}</b> </p>
+            {/* <div style={{width:"70%",margin:"auto"}} className="d-flex justify-content-center m-2"> */}
+            <Row style={{width:"70%",margin:"auto"}} > 
+             
+                <div>
+                  <div style={{ display: "flex", flexWrap: "wrap" }}>
+                    
+                      <Col lg="4" md="4" sm="6" xs="12">
         
-            <Card className="bgCard bg1 col-sm-6 col-lg-3">
+            <Card className="bgCard bg1">
+                
 
             <Card.Body>
                 <Card.Title>Customer Data Management</Card.Title>
-                {/* <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
-                </Card.Text>
+                
                 <Card.Link href="/addEmployee">Add</Card.Link>
                 <Card.Link href="/viewAllEmployees">Edit</Card.Link>
             </Card.Body>
             </Card>
-           <Card className="bgCard bg2 col-sm-6 col-lg-3">
-
-            <Card.Body>
-                <Card.Title>Loan Card Management</Card.Title>
-                {/* <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
-                </Card.Text> */}
-                <Card.Link href="/addEmployee">Add</Card.Link>
-                <Card.Link href="/viewAllEmployees">Edit</Card.Link>
-            </Card.Body>
-            </Card>
-            <Card className="bgCard bg3 col-sm-6 col-lg-3">
+            </Col>
+            <Col lg="4" md="4" sm="6" xs="12" >
+            <Card className="bgCard bg2 ">
 
             <Card.Body>
                 <Card.Title>Items Master Data</Card.Title>
-                {/* <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
-                </Card.Text>
+                
                 <Card.Link href="/addItem">Add</Card.Link>
                 <Card.Link href="/viewAllItems">Edit</Card.Link>
             </Card.Body>
             </Card>
-            <Card className="bgCard bg4 col-sm-6 col-lg-3">
-            <Card.Body>
-                <Card.Title>All items</Card.Title>
-                {/* <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
-                </Card.Text> */}
-                <Card.Link href="/addItem">Add</Card.Link>
-                <Card.Link href="/viewAllItems">Edit</Card.Link>
-            </Card.Body>
-            </Card>
-            <Card style={{ width: '10rem' }}>
+            </Col>
+            <Col lg="4" md="4" sm="6" xs="12">
+            <Card className="bgCard bg3">
             <Card.Body>
                 <Card.Title>Loan Card Data</Card.Title>
-                {/* <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
-                </Card.Text>
-                <Card.Link href="/addItem">Add</Card.Link>
-                <Card.Link href="/viewAllItems">Edit</Card.Link>
-            </Card.Body>
-            </Card>
-            <Card style={{ width: '10rem' }}>
-            <Card.Body>
-                <Card.Title>All items</Card.Title>
-                {/* <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
-                </Card.Text> */}
+               
                 <Card.Link href="/addLoanCard">Add</Card.Link>
                 <Card.Link href="/viewAllLoanCards">Edit</Card.Link>
             </Card.Body>
             </Card>
+            </Col>
+          
+            </div>
+            </div>
+            </Row>
         </div>
-        </div>
+        // </div>
     );
 }
