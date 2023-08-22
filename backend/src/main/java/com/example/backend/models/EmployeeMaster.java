1f78c34495715846fb435938bd9600d027a7c983
sukriti93em <141900702+sukriti93em@users.noreply.github.com>
2023-08-22 14:50:12 +0530

    added validation to employee master

--- backend/src/main/java/com/example/backend/models/EmployeeMaster.java
+++ backend/src/main/java/com/example/backend/models/EmployeeMaster.java
@@ -1,12 +1,13 @@
 package com.example.backend.models;
 
 import java.util.Date;
+
 import java.util.List;
 
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
@@ -16,6 +17,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 
 @Entity
 @Table(name="employee_master")
@@ -24,22 +29,33 @@ public class EmployeeMaster {
 	@Column(name="employee_id", nullable=false, length=6)
 	private String employeeId;
 	
+	@Valid
+	
+	
 	@Column(name="employee_name", length=20)
+	@NotBlank(message = "cannot be blank")
 	private String employeeName;
 	
+	@NotNull
+	@NotBlank(message ="designation cannot be empty")
 	@Column(name="designation", length=25)
 	private String designation;
 	
+	
 	@Column(name="department", length=25)
+	@NotBlank(message ="department cannot be empty")
 	private String department;
+	
 	
 	@Column
 	private char gender;
 	
+	@NotNull
 	@Column(name="date_of_birth")
 	@JsonFormat(pattern="yyyy-MM-dd")
 	private Date dateOfBirth;
 	
+	@NotNull
 	@Column(name="date_of_joining")
 	@JsonFormat(pattern="yyyy-MM-dd")
 	private Date dateOfJoining;
